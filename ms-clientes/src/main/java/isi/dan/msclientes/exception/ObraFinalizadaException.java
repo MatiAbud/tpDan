ab699306eae1bae09af7427b84bd53d47ae55136
MatiasA <matiabud@hotmail.com>
2024-08-04 13:00:05 -0300

    termino reglas de negocio cliente

---
+++ ms-clientes/src/main/java/isi/dan/msclientes/exception/ObraFinalizadaException.java
@@ -0,0 +1,7 @@
+package isi.dan.msclientes.exception;
+
+public class ObraFinalizadaException extends Exception {
+    public ObraFinalizadaException(String msg){
+        super(msg);
+    }
+}
--- ms-clientes/src/main/java/isi/dan/msclientes/servicios/ObraService.java
+++ ms-clientes/src/main/java/isi/dan/msclientes/servicios/ObraService.java
@@ -2,13 +2,15 @@ package isi.dan.msclientes.servicios;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import isi.dan.msclientes.dao.ClienteRepository;
 import isi.dan.msclientes.dao.ObraRepository;
+import isi.dan.msclientes.exception.ObraFinalizadaException;
 import isi.dan.msclientes.model.Cliente;
 import isi.dan.msclientes.model.EstadoObra;
 import isi.dan.msclientes.model.Obra;
@@ -31,6 +33,7 @@ public class ObraService {
     }
 
     public Obra save(Obra obra) {
+        obra.setEstado(EstadoObra.PENDIENTE);
         return obraRepository.save(obra);
     }
 
@@ -40,6 +43,15 @@ public class ObraService {
 
     public void deleteById(Integer id) {
         obraRepository.deleteById(id);
+    }
+
+    public Obra marcarObraComoHabilitada(Integer id) throws Exception {
+        Obra obra = obraRepository.findById(id).orElseThrow();
+
+        obra.setEstado(EstadoObra.HABILITADA);
+        obraRepository.save(obra);
+
+        return obra;
     }
 
     public Obra marcarObraComoFinalizada(Integer id) throws Exception {
@@ -61,16 +73,16 @@ public class ObraService {
     public List<Obra> obtenerObrasDeCliente(Integer idCliente) throws Exception {
         List<Obra> obras = obraRepository.findByClienteId(idCliente);
 
-        if (obras.isEmpty()) {
+        /*if (obras.isEmpty()) {
             throw new Exception("No se encontraron obras para el cliente con ID: " + idCliente);
-        }
+        }*/
 
         return obras;
     }
 
     // Otros métodos según necesidades
     public Obra cambiarEstadoObra(Integer idObra, EstadoObra nuevoEstado)
-            throws ObjectNotFoundException {
+            throws Exception {
 
         Obra obra = obraRepository.findById(idObra)
                 .orElseThrow();
@@ -85,7 +97,14 @@ public class ObraService {
         }
 
         // Chequeamos si el estado finalizado cambia a otro estado
-        if (obra.getEstado().equals(EstadoObra.FINALIZADA) && !nuevoEstado.equals(EstadoObra.FINALIZADA)) {
+        /*if (obra.getEstado().equals(EstadoObra.FINALIZADA) && !nuevoEstado.equals(EstadoObra.FINALIZADA)) {
+            try {
+                throw new Exception("No se puede cambiar el estado de una obra FINALIZADA");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }*/
+        if (obra.getEstado().equals(EstadoObra.FINALIZADA)) {
             try {
                 throw new Exception("No se puede cambiar el estado de una obra FINALIZADA");
             } catch (Exception e) {
@@ -102,11 +121,13 @@ public class ObraService {
             }
         }
         // No debe supera el maximo
-        if (nuevoEstado.equals(EstadoObra.HABILITADA)) {
+       /* if (nuevoEstado.equals(EstadoObra.HABILITADA)) {
             Cliente cliente = obra.getCliente();
-            int obrasHabilitadasCliente = obraRepository.countByClienteAndEstado(cliente,
-                    EstadoObra.HABILITADA);
-            if (obrasHabilitadasCliente >= cliente.getMaxObrasEnEjecucion()) {
+            /*List<Obra> obrasHabilitadasCliente = this.obtenerObrasDeCliente(cliente.getId());
+            obrasHabilitadasCliente.stream()
+                                    .filter(o -> o.getEstado().equals(EstadoObra.HABILITADA))
+                                    .collect(Collectors.toList());
+            if (obrasHabilitadasCliente.size() == cliente.getMaxObrasEnEjecucion()) {
                 try {
                     throw new Exception(
                             "No se pueden habilitar más obras para el cliente: " + cliente.getNombre()
@@ -115,7 +136,7 @@ public class ObraService {
                     e.printStackTrace();
                 }
             }
-        }
+        }*/
 
         // Actualizamos estado
         obra.setEstado(nuevoEstado);
@@ -126,7 +147,7 @@ public class ObraService {
         return obra;
     }
 
-    public Obra asignarObraACliente(Integer idCliente, Obra obra) throws ClassNotFoundException {
+    public Obra asignarObraACliente(Integer idCliente, Obra obra) throws Exception {
         // recuperar estado habilitado del cliente
         Cliente cliente = clienteRepository.findById(idCliente)
                 .orElseThrow(() -> new ClassNotFoundException("Cliente no encontrado con ID: " + idCliente));
@@ -136,12 +157,15 @@ public class ObraService {
         // throw new Exception("El cliente no está habilitado para asignar obras: " +
         // idCliente);
         // }
+        if(obra.getEstado()==EstadoObra.FINALIZADA){
+            throw new ObraFinalizadaException("La obra se encuentra finalizada");
+        }
+        else{
+            obra.setCliente(cliente);
+            obra = obraRepository.save(obra);
+            return obra;
+        }
 
-        // asociar la obra con el cliente y guardar
-        obra.setCliente(cliente);
-        obra = obraRepository.save(obra);
-
-        return obra;
     }
 
 }
